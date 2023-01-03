46e192188f59d46518180054bcc375a080f80d3a
Chaewon <ksdyys3@naver.com>
2023-01-04 01:01:12 +0900

    JavaScript-hadling events

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -6,23 +6,42 @@ function loadItems() {
 
 function displayItems(items) {
   const container = document.querySelector(".items");
+
   container.innerHTML = items.map((item) => creatHTMLString(item)).join("");
 }
 
-//main
 function creatHTMLString(item) {
   return `
-    <li class="item">
-        <img src=${item.image} alt=${item.type} class="item__thumbnail" />
-        <span class="item__description">${item.gender}, ${item.size}</span>
-    </li>
-    `;
+  <li class="item">
+      <img src=${item.image} alt=${item.type} class="item__thumbnail" />
+      <span class="item__description">${item.gender}, ${item.size}</span>
+  </li>
+  `;
+}
+
+function onButtonClick(e, items) {
+  const dataset = e.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
+  console.log(dataset);
+  if (key == null || value == null) {
+    return;
+  }
+
+  displayItems(items.filter((item) => item[key] == value));
+}
+
+function setEventListeners(items) {
+  const logo = document.querySelector(".logo");
+  const buttons = document.querySelector(".buttons");
+  logo.addEventListener("click", () => displayItems(items));
+  console.log(buttons);
+  buttons.addEventListener("click", (e) => onButtonClick(e, items));
 }
 
 loadItems()
   .then((items) => {
-    console.log(items);
     displayItems(items);
-    //setEventListener(items);
+    setEventListeners(items);
   })
   .catch(console.log);
