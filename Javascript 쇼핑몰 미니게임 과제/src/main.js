3ec56a3fb880f43b729306b03ebf75294ca95da5
Chaewon <ksdyys3@naver.com>
2023-01-04 00:31:07 +0900

    JavaScript-display items

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -4,11 +4,25 @@ function loadItems() {
     .then((json) => json.items);
 }
 
+function displayItems(items) {
+  const container = document.querySelector(".items");
+  container.innerHTML = items.map((item) => creatHTMLString(item)).join("");
+}
+
 //main
+function creatHTMLString(item) {
+  return `
+    <li class="item">
+        <img src=${item.image} alt=${item.type} class="item__thumbnail" />
+        <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
+}
+
 loadItems()
   .then((items) => {
     console.log(items);
-    // displayItems(items);
-    // setEventListener(items);
+    displayItems(items);
+    //setEventListener(items);
   })
   .catch(console.log);
