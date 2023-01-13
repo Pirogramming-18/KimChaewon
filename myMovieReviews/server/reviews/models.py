19e9115901970a3b93581d1cb1f0d717883f889d
Chaewon <ksdyys3@naver.com>
2023-01-14 03:10:32 +0900

    잠깐만...

--- myMovieReviews/server/reviews/models.py
+++ myMovieReviews/server/reviews/models.py
@@ -5,10 +5,14 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 class Movie(models.Model):
     #제목, 개봉 년도, 장르, 별점
+    #영화 제목, 감독, 주연, 장르, 별점, 러닝타임, 리뷰 내용
     nameMv=models.CharField(max_length=64)
-    yearsMv=models.CharField(max_length=32,help_text='개봉 년도')
+    yearsMv=models.CharField(max_length=32)
     genreMv=models.CharField(max_length=32)
     gradeMv= models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(5)],help_text="0~5사이 값으로 입력하세요")
+    timeMv=models.IntegerField()
+    actorMv=models.CharField(max_length=64)
+    reviewMv=models.TextField()
     
 
 
