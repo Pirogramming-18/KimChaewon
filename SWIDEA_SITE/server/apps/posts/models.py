0ecee97c61f94c53fca4d4fc1201926166545efd
Chaewon <ksdyys3@naver.com>
2023-01-19 01:27:59 +0900

    커밋4

--- SWIDEA_SITE/server/apps/posts/models.py
+++ SWIDEA_SITE/server/apps/posts/models.py
@@ -2,22 +2,20 @@ from django.db import models
 from django.utils import timezone
 
 
-class Post(models.Model):
-    TOOL_CHOICE=[('django',"Django"),
-    ('react','React'),
-    ('spring','Spring'),
-    ('node.js','Node.js'),
-    ('java','Java'),
-    ('c++','C++')
-    ]
-    title=models.CharField(max_length=64)
-    image=models.ImageField(blank=True, upload_to='posts/%Y%m%d')
-    content = models.TextField()
-    interest=models.IntegerField(default=0)
-    devtool=models.CharField(max_length=64,choices=TOOL_CHOICE)
-    timestamp = models.DateTimeField(default=timezone.now) #만들어진 시간
-
 class ToolList(models.Model):
     name=models.CharField(max_length=64)
     kind=models.CharField(max_length=64)
     content = models.TextField()
+
+class Post(models.Model):
+    title=models.CharField(max_length=64)
+    tool= models.ForeignKey(ToolList, on_delete=models.CASCADE)
+    image=models.ImageField(blank=True, upload_to='posts/%Y%m%d')
+    content = models.TextField()
+    interest=models.IntegerField(default=0)
+    toollist=ToolList.objects.all()
+    TOOL_CHOICE=[]
+    for tooll in toollist:
+        TOOL_CHOICE.append((tooll,tooll))
+    devtool=models.CharField(max_length=64,choices=TOOL_CHOICE)
+    timestamp = models.DateTimeField(default=timezone.now) #만들어진 시간
